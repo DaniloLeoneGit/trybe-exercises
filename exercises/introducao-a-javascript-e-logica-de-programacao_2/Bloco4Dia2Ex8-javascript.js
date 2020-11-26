5ed04a3554951587766b0da49e9d6ea5bf0abc8d
DaniloEstevam <danilo.aaab@gmail.com>
2020-11-26 20:51:39 -0300

    exercicio 8 concluido com basteante gasto de neuronios

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/Bloco4Dia2Ex8-javascript.js
@@ -0,0 +1,17 @@
+// 8 -  Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
+
+let numerosProArray = []
+
+for (let array = 1; array <= 25; array += 1) {
+    numerosProArray.push(array); 
+}
+
+console.log(numerosProArray);
+
+// let numbers = [];
+
+// for (let i = 1; i <= 25; i += 1) {
+//   numbers.push(i);
+// }
+
+// console.log(numbers);
