65a005233cdac81c6a5d5a0da24ae7ddaa0965c5
DaniloEstevam <danilo.aaab@gmail.com>
2020-11-27 18:40:15 -0300

    Primeiro exercicio , impossivel do dia = fazer quadrado de asterisco

--- Blo4D3EX1.js
+++ Blo4D3EX1.js
@@ -1,1 +1,15 @@
-primeiro commit 
+// faça um programa 
+//dado um valor n 
+//seja n > 1 , 
+//imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo
+
+
+let ladoQadrado = 10
+
+for (let index = 0; index < ladoQadrado; index += 1) {
+    let linha = '';
+    for(let coluna = 0; coluna < ladoQadrado ; coluna +=1) {
+        linha = linha + '*';
+    }
+    console.log(linha)
+}
