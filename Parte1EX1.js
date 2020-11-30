422edd10ddf7bb204e74bed68c039fec5369abb4
DaniloEstevam <danilo.aaab@gmail.com>
2020-11-30 18:51:10 -0300

    Exercicio 3 concluido

--- Parte1EX1.js
+++ Parte1EX1.js
@@ -3,10 +3,15 @@ let info = {
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
   };
-
+//Ex 1
  // console.log('Bem-vinda, ' + info.personagem)
 
+//ex 2
+ info['recorrente'] = 'sim'
+ //console.log(info)
 
- //console.log('personagem: ' + info.personagem + ' origem: ' + info.origem + ' nota: ' + info.nota )
-info['recorrente'] = 'sim'
- console.log(info)
+ //ex3
+
+ for (let caracteristicasDoPersonagem in info) {
+    console.log(caracteristicasDoPersonagem)
+ }
