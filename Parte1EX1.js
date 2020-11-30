aa67ee28617f3f3acbe156430c279407abd8fa0f
DaniloEstevam <danilo.aaab@gmail.com>
2020-11-30 18:44:34 -0300

    Exercicio 1 comentado + exercicio 2 concluido

--- Parte1EX1.js
+++ Parte1EX1.js
@@ -4,4 +4,9 @@ let info = {
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
   };
 
-  console.log('Bem-vinda, ' + info.personagem)
+ // console.log('Bem-vinda, ' + info.personagem)
+
+
+ //console.log('personagem: ' + info.personagem + ' origem: ' + info.origem + ' nota: ' + info.nota )
+info['recorrente'] = 'sim'
+ console.log(info)
